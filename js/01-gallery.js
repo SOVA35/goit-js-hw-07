3b9a36e15ec40c934808e9cdb54dc0dc33ce6b0e
SOVA35 <kushnerandrew2604@gmail.com>
2023-06-10 15:02:05 +0300

    added files to repository

---
+++ js/01-gallery.js
@@ -0,0 +1,50 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const galleryContainer = document.querySelector('.gallery');
+  
+const markupGallery = createGalleryItemsMarkup(galleryItems);
+
+
+// galleryContainer.preventDefault();
+
+function createGalleryItemsMarkup(galleryItems) {
+   return galleryItems.map(({ original, preview, description }) => {
+        return `
+    <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>
+    `
+    }).join("");
+}
+ galleryContainer.insertAdjacentHTML('beforeend', markupGallery);
+
+
+galleryContainer.addEventListener('click', onGalleryContainerClick);
+
+function onGalleryContainerClick (evt) {
+    evt.preventDefault();
+  if (!evt.target.classList.contains("gallery__item")) {
+    return;
+  }
+  onGalleryContainerClick = basicLightbox.create(`
+<img src="${evt.dataset.sourse}" width="800" height="600">
+`)
+  onGalleryContainerClick.show();
+
+ 
+}
+
+
+
+
+   
+
+
